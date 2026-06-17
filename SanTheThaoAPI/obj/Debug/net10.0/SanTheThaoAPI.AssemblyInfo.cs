fad16efe792f26eef57a9d0858beff701ee7cefb
cuongpipe <cuongpipe@gmail.com>
2026-06-17 21:03:44 +0700

    up

--- SanTheThaoAPI/obj/Debug/net10.0/SanTheThaoAPI.AssemblyInfo.cs
+++ SanTheThaoAPI/obj/Debug/net10.0/SanTheThaoAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SanTheThaoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63758a21377edcf1b496b5cc13cdc18f98f18064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+445e326bcc8f8cbcd34193489ae82a8c2f712193")]
 [assembly: System.Reflection.AssemblyProductAttribute("SanTheThaoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SanTheThaoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
